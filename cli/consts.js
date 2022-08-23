224ebd03ef48d0ac2c67124d0006dee7081aeb1a
Andrew Rubin <arubin78@gmail.com>
2022-08-23 15:16:57 -0700

    feat(cli): add template flag, refactor accordingly

--- cli/consts.js
+++ cli/consts.js
@@ -9,7 +9,11 @@ const CONSTS = {
   QUESTIONS: [
     {
       id: "directory",
-      text: "Enter a directory path for the project: ",
+      text: 'Enter a directory name for the project. Use a relative path; to use the current directory, type "." : ',
+    },
+    {
+      id: "template",
+      text: "Do you have a Github branch to use as a project template? (Optional; enter to skip): ",
     },
   ],
 };
