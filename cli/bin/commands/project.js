fbe47ceb80ff9a4c223e23d57b34b50a6e37e8f0
Andrew Rubin <arubin78@gmail.com>
2022-09-06 16:06:23 -0700

    refactor(cli): move instruction helper into lib/; remove cd instruction when directory is current directory

--- cli/bin/commands/project.js
+++ cli/bin/commands/project.js
@@ -2,14 +2,14 @@ import { mkdir, readFile, writeFile } from "fs/promises";
 import fse from "fs-extra";
 import path from "path";
 import * as url from "url";
-import chalk from "chalk";
 
 import CONSTS from "../../consts.js";
 
-import mergeTemplateRepo from "../../lib/merge-template-repo.js";
-import mergePkgProperties from "../../lib/merge-package-properties.js";
-import prompt from "../../lib/prompt.js";
 import log, { killSpinner } from "../../lib/log.js";
+import mergePkgProperties from "../../lib/merge-package-properties.js";
+import mergeTemplateRepo from "../../lib/merge-template-repo.js";
+import printNextSteps from "../../lib/print-next-steps.js";
+import prompt from "../../lib/prompt.js";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const { pathExistsSync, copySync } = fse;
@@ -17,16 +17,6 @@ let projectConfig = { ...CONSTS.CONFIG_DEFAULTS };
 let usedCustomConfig = false;
 
 const getQuestion = (id) => CONSTS.QUESTIONS.find((q) => q.id === id).text;
-
-const printNextSteps = (dir) => {
-  log("ok", "Project setup complete.");
-  log(
-    "msg",
-    `\nTo get started, run: \n\ncd ${dir} \nnvm use ${chalk.dim(
-      "(if using nvm)"
-    )} \nnpm install \nnpm start`
-  );
-};
 
 const project = async (directory, options) => {
   let { template } = options;
---
+++ cli/lib/print-next-steps.js
@@ -0,0 +1,15 @@
+import log from "./log";
+
+const printNextSteps = (dir) => {
+  const cdInstruction = dir === "." ? "" : `\ncd ${dir} `
+
+  log("ok", "Project setup complete.");
+  log(
+    "msg",
+    `\nTo get started, run: \n${cdInstruction}\nnvm use ${chalk.dim(
+      "(if using nvm)"
+    )} \nnpm install \nnpm start`
+  );
+};
+
+export default printNextSteps
