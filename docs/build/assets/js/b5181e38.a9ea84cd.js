18191901a4e024878351f75ed6f048ecebc9013d
Andrew Rubin <arubin78@gmail.com>
2022-11-21 12:58:16 -0800

    chore(docs): add built docs to version control

---
+++ docs/build/assets/js/b5181e38.a9ea84cd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_corgi_docs=self.webpackChunk_corgi_docs||[]).push([[566],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ docs/build/assets/js/bd5b7547.6e7e64d7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_corgi_docs=self.webpackChunk_corgi_docs||[]).push([[73],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
