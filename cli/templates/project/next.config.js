e02301bbf1dd29c52efb28408a607e3295d34d3d
Andrew Rubin <arubin78@gmail.com>
2023-04-11 17:54:28 -0700

    feat(CLI project template): add webpack config for subpath resolvers

--- cli/templates/project/next.config.js
+++ cli/templates/project/next.config.js
@@ -1,9 +1,22 @@
+const path = require("path")
 const withPlugins = require("next-compose-plugins")
 const yaml = require("next-plugin-yaml")
+
+const resolve = (dirPath) => path.resolve(__dirname, dirPath)
 
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@local/components": resolve("src/components"),
+      "@local/context": resolve("src/context"),
+      "@local/hooks": resolve("src/hooks"),
+      "@local/utils": resolve("src/utils"),
+    }
+    return config
+  },
 }
 
 module.exports = withPlugins([[yaml], nextConfig])
