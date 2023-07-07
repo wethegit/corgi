fc4e91f8a9b3052a4f29b298e7c9c1e9e9deb2fb
Marlon Ugocioni Marcello <marlon.marcello@gmail.com>
2023-07-06 17:19:51 -0700

    fix: ensures only strings are accepts

--- cli/templates/project/src/utils/classnames.js
+++ cli/templates/project/src/utils/classnames.js
@@ -1,6 +1,7 @@
 export function classnames(classes) {
   if (!classes) return null
   if (typeof classes === 'string') return classes
+  if (!Array.isArray(classes)) return null;
 
-  return classes.flat().filter(c => Boolean(c)).join(" ")
+  return classes.flat().filter(c => typeof c === 'string').join(" ")
 }
