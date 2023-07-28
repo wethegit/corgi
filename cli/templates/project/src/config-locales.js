4a02bf0b32aad810fe1921e6c80cfc8f89c8e6fc
Andrew Rubin <arubin78@gmail.com>
2023-07-28 11:43:50 -0700

    refactor(locale config): organize file, derive locale strings from Map

--- cli/templates/project/src/config-locales.js
+++ cli/templates/project/src/config-locales.js
@@ -1,51 +1,47 @@
+/**
+ * Add your required locales here, along with any mapping to specifically-formatted
+ * strings. For example, if your locale string differs from the ISO standard
+ * abbreviation, you can add that here.
+ *   
+ * The mapped object value will be available to both the Page's pageProps and the
+ * useLocales hook as `localeMap`. If the locale is missing from the map then
+ * localeMaps will be an empty object.
+ */
+const localeMap = new Map([
+  [
+    "en",
+    {
+      language: "en-US",
+    },
+  ],
+  [
+    "es",
+    {
+      language: "es-ES",
+    },
+  ],
+  [
+    "fr",
+    {
+      language: "fr-FR",
+    },
+  ],
+]);
+
+
+/**
+ * Make just the locale slugs available (i.e. ["en", "fr", etc.]):
+ */
+const locales = Array.from(localeMap.keys())
+
 module.exports = {
   defaultLocale: "en",
+  locales,
+  localeMap,
 
-  /*
-    locales here are not used to generate pages,
-    however is it useful to store the languages
-    you intend to support here.
-  */
-
-  locales: ["en", "es", "fr"],
-
-  /*
-    Both <html lang=""> and <Link hreflang ... />
-    tags are auto generated for every page. The
-    value for those will default to the locale
-    unless an alternative in the localeMap
-    (see below) is defined here.
-  */
-
+  /** Both <html lang=""> and <Link hreflang ... /> tags are auto generated for every
+   * page. The value for those will default to the locale slug (i.e. "en", "fr", etc.),
+   * unless an alternative property defined the localeMap is specified here:
+   */
   langValue: "language",
-
-  /*
-    if you need to map your locales to specific
-    alternatives then add them here. The mapped
-    object will be available to the page and
-    useLocales hook as localeMaps. If the locale
-    is missing from the map then localeMaps will
-    be an empty object
-  */
-
-  localeMap: new Map([
-    [
-      "en",
-      {
-        language: "en-US",
-      },
-    ],
-    [
-      "es",
-      {
-        language: "es-ES",
-      },
-    ],
-    [
-      "fr",
-      {
-        language: "fr-FR",
-      },
-    ],
-  ]),
 }
