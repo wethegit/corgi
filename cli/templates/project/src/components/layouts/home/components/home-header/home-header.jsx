2337d4cbd0f753b08a293b851a65446ee710695e
Marlon Ugocioni Marcello <marlon.marcello@gmail.com>
2023-08-17 14:11:05 -0700

    feature: adds spacing classes (#67)
    * feature: adds spacing classes
    
    * feat(style utils): add child-spacing utils
    
    ---------
    
    Co-authored-by: Andrew Rubin <arubin78@gmail.com>

--- cli/templates/project/src/components/layouts/home/components/home-header/home-header.jsx
+++ cli/templates/project/src/components/layouts/home/components/home-header/home-header.jsx
@@ -5,7 +5,7 @@ export function HomeHeader() {
 
   return (
     <header className="content-spacing">
-      <h1>{page.title}</h1>
+      <h1 className="margin-bottom-2 margin-bottom-large-2">{page.title}</h1>
       <p>{page.subtitle}</p>
     </header>
   )
