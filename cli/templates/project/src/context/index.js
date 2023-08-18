9b484b67aa95159d756d328ef6ce4420355b080a
Andrew Rubin <arubin78@gmail.com>
2023-08-18 11:10:22 -0700

    chore(PageTransition): remove component

--- cli/templates/project/src/context/index.js
+++ cli/templates/project/src/context/index.js
@@ -1,7 +1,3 @@
 export { LocaleProvider, LocaleContext } from "./locale-context.js"
 export { PageProvider, PageContext } from "./page-context.js"
-export {
-  SiteStateProvider,
-  SiteStateContext,
-  PAGE_TRANSITION_STATE,
-} from "./site-state-context.js"
+export { SiteStateProvider, SiteStateContext } from "./site-state-context.js"
