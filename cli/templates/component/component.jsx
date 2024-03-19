3f03e7d50aa3b53db330f426add3b7d85ca74112
Andrew Rubin <arubin78@gmail.com>
2024-03-19 16:41:38 -0700

    fix(CLI templates): remove opinions from base component

--- cli/templates/component/component.jsx
+++ cli/templates/component/component.jsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react"
 
 import { useLocale } from "@local/hooks"
-import { classnames } from "@local/utilities"
 
-import styles from "./COMPONENT_SLUG.module.scss"
-
-export function COMPONENT_NAME({ className, ...props }) {
+export function COMPONENT_NAME({ ...props }) {
   const { globals, page } = useLocale()
 
   return (
-    <div className={classnames([styles.wrap, className])} {...props}>
+    <div {...props}>
       <code>COMPONENT_NAME</code>!
     </div>
   )
