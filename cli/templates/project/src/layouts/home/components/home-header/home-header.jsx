8c0d7d419b5012ba12536708133ae31e1ec8fe80
Andrew Rubin <30575213+andrewrubin@users.noreply.github.com>
2024-04-30 16:36:05 -0700

    chore(HomeHeader): remove non-existent spacing utils (#126)

--- cli/templates/project/src/layouts/home/components/home-header/home-header.jsx
+++ cli/templates/project/src/layouts/home/components/home-header/home-header.jsx
@@ -4,9 +4,8 @@ export function HomeHeader() {
   const { page } = useLocale()
 
   return (
-    <header className="content-spacing">
-      <h1 className="margin-bottom-2 margin-bottom-large-2">{page.title}</h1>
-
+    <header>
+      <h1>{page.title}</h1>
       <p>{page.subtitle}</p>
     </header>
   )
